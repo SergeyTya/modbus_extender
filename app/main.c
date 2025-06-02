50485a154bf8de3edbf30482e11ec8d6fb2d3ad4
Sergey_T14 <sergey_ty@mail.ru>
2025-06-02 11:13:48 +0300

    first start

--- app/main.c
+++ app/main.c
@@ -15,17 +15,17 @@ bool proces_data(uint8_t * data, size_t len, uint8_t adr);
 
 void main(){
 
-    nvic_config();
-    com_usart_init();
-    gpio_init();
+    // nvic_config();
+    // com_usart_init();
+     gpio_init();
 
-    addr = hw_get_addres();
+    // addr = hw_get_addres();
 
     int main_cnt = 0;
 
     while(1){
         main_cnt++;
-        if(main_cnt > (int) 16e6){
+        if(main_cnt > (int) 16e5){
             main_cnt = 0;
             gpio_bit_toggle(GPIOC, GPIO_PIN_13);
         }
